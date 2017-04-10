ae40fa5f21cbe8cdc49163e001affaee99a6652c
Jordan Hand <jordanhand@comcast.net>
2017-04-09 23:18:55 -0700

    changed gulpfile

--- rpi/gulpfile.js
+++ rpi/gulpfile.js
@@ -4,6 +4,6 @@ var nodemon = require('gulp-nodemon');
 gulp.task("run-dev", [], () => {
     nodemon({
         script: 'server.js',
-        args: ['-d', '-e', '-b']
+        args: ['-d', '-e']
     }).on("start");
 })
